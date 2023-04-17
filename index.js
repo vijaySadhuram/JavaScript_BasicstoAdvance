b9f3d1cf4d30a8457e61dd47783299d5569d4ba7
Vijay <vijaysadhuram4868@gmail.com>
2023-04-17 14:37:24 +0530

    js Objects

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-let a="123"
+/*let a="a";
 console.log(Number(a));
 let b=Number(a);
 console.log(typeof b);
 console.log(b)
+//number to string
+let num=123;
+console.log(String(num));
+//String method
+let s="my name is vjiay";
+*/
+/*
+Arrays and Objects*
+*/
+// let arr=[1,3,4,6,7,8,];
+
+// console.log(arr);
+
+// console.log(arr.slice(1,3));
+const car={
+    carName:"bmw",
+    carSpeed:"12km/pr",
+};
+car.carColor="Red";
+console.log(car.carName);
