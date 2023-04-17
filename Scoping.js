060bb71dbd1858eff5f4ea00da1d2f9e116fc34b
Vijay <vijaysadhuram4868@gmail.com>
2023-04-17 16:30:57 +0530

    Scoping in JS

---
+++ Scoping.js
@@ -0,0 +1,11 @@
+const a=30;
+{
+    // const b=40;
+}
+// console.log(a,b);
+// undefined error above code
+var c=30;
+{
+    var d=50;
+}
+console.log(c,d);
--- function.js
+++ function.js
@@ -1,9 +1,20 @@
-let a=40,b=60,c=90;
-function myFunction(){
-    // let b=90;
-    console.log(b);
-    let d=10;
-}
-console.log(c);
+// let a=40,b=60,c=90;
+// function myFunction(){
+//     // let b=90;
+//     console.log(b);
+//     let d=10;
+// }
+// console.log(c);
 
-myFunction(10,50,60);
+// myFunction(10,50,60);
+function sumOfarrayOfElement(arr){
+    let sum=0;
+    for( i=0;i<arr.length;i++){
+        sum=sum+arr[i];
+
+    }
+    return sum;
+}
+let arr=[1,2,3]
+let sumArray=sumOfarrayOfElement(arr);
+console.log(sumArray);
